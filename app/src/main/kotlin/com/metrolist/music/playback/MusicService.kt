1f2f63f3a4aea19524b4c77979c38e0ad6edc035
SimoneFelici <felicisimone9@gmail.com>
2026-07-06 22:01:51 +0200

    fix: fix persisting shuffle toggle not working

--- app/src/main/kotlin/com/metrolist/music/playback/MusicService.kt
+++ app/src/main/kotlin/com/metrolist/music/playback/MusicService.kt
@@ -1206,6 +1206,7 @@ class MusicService :
                                 playQueue(
                                     queue = restoredQueue,
                                     playWhenReady = false,
+                                    restoringQueue = true,
                                 )
                             }
                         }
@@ -1761,6 +1762,7 @@ class MusicService :
     fun playQueue(
         queue: Queue,
         playWhenReady: Boolean = true,
+        restoringQueue: Boolean = false,
     ) {
         if (!playerInitialized.value) {
             Timber.tag(TAG).w("playQueue called before player initialization, queuing request")
@@ -1774,8 +1776,7 @@ class MusicService :
         currentQueue = queue
         queueTitle = null
         val persistShuffleAcrossQueues = dataStore.get(PersistentShuffleAcrossQueuesKey, false)
-        val previousShuffleEnabled = player.shuffleModeEnabled
-        if (!persistShuffleAcrossQueues) {
+        if (!persistShuffleAcrossQueues && !restoringQueue) {
             player.shuffleModeEnabled = false
         }
         originalQueueSize = 0
